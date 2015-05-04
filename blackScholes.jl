3cc139b4e3ecda1bc4402637000c458dcf8991e8
El-Hadi <doudido78@gmail.com>
2015-05-04 05:46:34 +0100

    ajout de la formule d

--- blackScholes.jl
+++ blackScholes.jl
@@ -11,6 +11,19 @@ function blackScholesCall(S0,k,r,σ,t)
  return S0*cdf(Normal(),d1)-k*exp(-r*t)*cdf(Normal(),d2)
 
 
+
+
+end
 #princing d'un put par la formule de black et scholes
+function blackScholesPut(S0,k,r,σ,t)
+
+   d1=(log(S0/k)+(r+(σ^2)/2)*t)/(σ*sqrt(t))
+   d2=d1-σ*sqrt(t)
+
+ return -S0*cdf(Normal(),-d1)+k*exp(-r*t)*cdf(Normal(),-d2)
+
+
+
+
 
 end
