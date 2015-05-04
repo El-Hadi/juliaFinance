3cfd1795f14a7c21ec339b72dfa0426883af023f
El-Hadi <doudido78@gmail.com>
2015-05-04 10:11:47 +0200

    ajout du fichier CorporateFinance.jl

---
+++ CorporateFinance.jl
@@ -0,0 +1,25 @@
+function échéancierAnnuité(M,n,i)
+  A=M/((1-(1+i)^-n)/i)
+  eche=zeros(n,5)
+  eche[1,1]=M
+  eche[:,2]=A
+  eche[1,3]=eche[1,1]*i
+  eche[1,4]=eche[1,2]-eche[1,3]
+  eche[1,5]=eche[1,1]-eche[1,4]
+  for j in 2:n
+  eche[j,1]=eche[j-1,5]
+  eche[j,3]=i*eche[j,1]
+  eche[j,4]=eche[j,2]-eche[j,3]
+  eche[j,5]=eche[j,1]-eche[j,4]
+
+
+
+  end
+
+
+
+
+
+  return eche
+end
+
