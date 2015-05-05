deb5394f0b26fbb0d50743727c2c6756022f2074
El-hadi MEDEGHRI <doudido78@gmail.com>
2015-05-05 19:27:46 +0200

    correction d'erreur de merge

--- obligation.jl
+++ obligation.jl
@@ -1,5 +1,4 @@
 
-
 # calcul du prix d'une obligation
 
 # c coupon
@@ -18,3 +17,25 @@ end
 
 
 
+# calcul de la duration d'une obligation
+function duration(c,M,t,i)
+
+  temps=[0.5:0.5:t]
+  cf=c*ones(length(temps))
+  cf[end]=cf[end]+M
+  d=sum((cf.*temps)./(1+i).^temps)
+  return d/prixObligation(c,M,t,i)
+
+end
+
+
+
+
+#calcul de la convexité
+function convexité(c,M,t,i)
+  temps=[0.5:0.5:t]
+  cf=c*ones(length(temps))
+  cf[end]=cf[end]+M
+  d=sum((cf.*(temps+temps.^2))./(1+i).^temps)
+  return d/(prixObligation(c,M,t,i)*(1+i)^2)
+end
