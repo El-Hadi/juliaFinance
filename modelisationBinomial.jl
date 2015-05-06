7a324bdd47f2868bc66114a6de0afbe1f0077f69
El-Hadi <doudido78@gmail.com>
2015-05-06 12:01:42 +0200

    ajout de la fonction forwardLattice avec bug

--- modelisationBinomial.jl
+++ modelisationBinomial.jl
@@ -16,3 +16,25 @@ function arbreBinomial(s,u,d,t)
 
 
 end
+
+
+function forwardLatice(notionel,t,q, s, u, d)
+  fo=zeros(t,t)
+  fo[:,end]=notionel
+  r=arbreBinomial(s,u,d,t)
+  for j in t-1:-1:1
+    for i in t-j+1:t
+       #fo[i,j]=i+j
+      fo[i,j]= (q*fo[i-1,j+1]+(1-q)*fo[i,j+1])/(1+r[i,j])
+
+    end
+  end
+
+  #fo=fo./r
+  return fo
+
+
+end
+
+forwardLatice(100.,10,0.5, 0.05,1.1,0.9)
+
